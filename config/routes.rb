9b9c265c8e8a4e03c225f80d51d9752c205a6d02
Faiz <tenkois@gmail.com>
2018-12-02 15:04:00 +0800

    set video controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :videos
   get 'pages/about'
   get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
